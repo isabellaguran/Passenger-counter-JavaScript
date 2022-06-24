abbf3923d2c101915e9ae41cb12e13afae179e2e
Isabella <49249229+isabellaguran@users.noreply.github.com>
2022-06-24 12:46:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 //listen for clicks on increment button
 //increment the count variables when the button is clicked (log it out)
 //change the count-el in the HTML to reflect the new count
+// 1. Grab the save-el paragrah and store it in a variable called saveEl
+    let saveEl = document.getElementById("save-el")
 let countEl= document.getElementById("count-el") //we create a variable that looks into the html for the id "count-el" //also what is inside () means we pass in the argument
 console.log(countEl) //we check it to verify if it shows the id from the html
 
@@ -15,7 +17,13 @@ function increment(){
 
 //Create a function, save(), which logs out the count when it's called
 function save() {
+    // 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
+    let countStr = count + " - "
+    // 3. Render the variable in the saveEl using innerText
+    saveEl.innerText += countStr
+     // NB: Make sure to not delete the existing content of the paragraph
+    
     console.log(count)
 }
 
-
+console.log(saveEl)
