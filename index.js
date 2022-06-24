6bb8fda80f6e8f79b2d025e143b153e3fe9d5996
Isabella <49249229+isabellaguran@users.noreply.github.com>
2022-06-24 12:54:32 +0300

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function save() {
     // 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
     let countStr = count + " - "
     // 3. Render the variable in the saveEl using innerText
-    saveEl.innerText += countStr
+    saveEl.textContent += countStr   //no inner.Text so we don`t have spaces,we saw it on MDM
      // NB: Make sure to not delete the existing content of the paragraph
     
     console.log(count)
